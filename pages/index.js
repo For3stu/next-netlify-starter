afb62368d6d6c1f1e5a72f03ec6fbde32d3b3f77
Bartosz Lewandowski <barteklew123@gmail.com>
2022-05-11 21:59:56 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Where is Jajko!?</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Find him..." />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
